6e088348c214970f0c575845b8450ba46e483604
TheWezzz <wesley.vanvliet@student.hu.nl>
2022-09-09 18:00:27 +0200

    practicum 1,1 done

--- practicum-print-asm/main.cpp
+++ practicum-print-asm/main.cpp
@@ -1,26 +1,22 @@
 #include "hwlib.hpp"
 
-void uart_put_char( char c ){
-   hwlib::cout << c;
+extern "C" {
+void print_asciz(const char* s);
+void uart_put_char(char c) {
+    hwlib::cout << c;
+}
 }
 
-void print_asciz( const char * s ){
-   while( *s != '\0'){
-      uart_put_char( *s );
-      ++s;
-   }
+void application() {
+    print_asciz( "Hello world, the ANSWER is 42! @[]`{}~\n" );
 }
 
-void application(){
-   print_asciz( "Hello world, the ANSWER is 42! @[]`{}~\n" );
-}
+int main(void) {
 
-int main( void ){
+    namespace target = hwlib::target;
 
-   namespace target = hwlib::target;
+    // wait for the PC console to start
+    hwlib::wait_ms( 2000 );
 
-   // wait for the PC console to start
-   hwlib::wait_ms( 2000 );
-
-   application();
+    application();
 }
