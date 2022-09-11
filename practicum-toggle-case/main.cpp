9c2e05bd2deddf3f3ce2357ce5ae4bd239eadd10
TheWezzz <wesley.vanvliet@student.hu.nl>
2022-09-11 23:23:31 +0200

    practicum 1,3

---
+++ practicum-toggle-case/main.cpp
@@ -0,0 +1,19 @@
+#include "hwlib.hpp"
+
+extern "C" {
+void application();
+void print_asciz(const char* s);
+void uart_put_char(char c) {
+    hwlib::cout << c;
+}
+}
+
+int main(void) {
+
+    namespace target = hwlib::target;
+
+    // wait for the PC console to start
+    hwlib::wait_ms( 2000 );
+
+    application();
+}
