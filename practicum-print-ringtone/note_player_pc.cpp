c5a323a2e22e2b485d652c6f07e3931cd702f19f
TheWezzz <wesley.vanvliet@student.hu.nl>
2022-09-20 12:10:15 +0200

    practicum 2,3 implemented switch case for note cout

--- practicum-print-ringtone/note_player_pc.cpp
+++ practicum-print-ringtone/note_player_pc.cpp
@@ -1,5 +1,6 @@
 #include "hwlib.hpp"
 #include "note_player_pc.hpp"
+#include "iostream"
 
 
 void note_player_pc::play(const note& n) {
@@ -7,10 +8,57 @@ void note_player_pc::play(const note& n) {
         hwlib::wait_us( n.duration );
 
     } else {
-//        auto half_period = 1'000'000 / (2 * n.frequency);
         auto end = hwlib::now_us() + n.duration;
         do {
-            hwlib::cout << n.duration;
+            static const int f = n.frequency;
+            switch(f){
+                case 440:
+                    hwlib::cout << "A4" << hwlib::endl;
+                    break;
+                case 466:
+                    hwlib::cout << "A4s" << hwlib::endl;
+                    break;
+                case 494:
+                    hwlib::cout << "B4" << hwlib::endl;
+                    break;
+                case 523:
+                    hwlib::cout << "C5" << hwlib::endl;
+                    break;
+                case 554:
+                    hwlib::cout << "C5s" << hwlib::endl;
+                    break;
+                case 587:
+                    hwlib::cout << "D5" << hwlib::endl;
+                    break;
+                case 622:
+                    hwlib::cout << "D5s" << hwlib::endl;
+                    break;
+                case 659:
+                    hwlib::cout << "E5" << hwlib::endl;
+                    break;
+                case 698:
+                    hwlib::cout << "F5" << hwlib::endl;
+                    break;
+                case 740:
+                    hwlib::cout << "F5s" << hwlib::endl;
+                    break;
+                case 784:
+                    hwlib::cout << "G5" << hwlib::endl;
+                    break;
+                case 830:
+                    hwlib::cout << "G5s" << hwlib::endl;
+                    break;
+                case 880:
+                    hwlib::cout << "A5" << hwlib::endl;
+                    break;
+                case 932:
+                    hwlib::cout << "A5s" << hwlib::endl;
+                    break;
+                case 987:
+                    hwlib::cout << "B5" << hwlib::endl;
+                    break;
+
+            }
         } while (end > hwlib::now_us());
     }
 }
