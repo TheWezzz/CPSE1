e4bf928903b3642ff7cfd211d1024ada4591f4f4
TheWezzz <wesley.vanvliet@student.hu.nl>
2022-10-11 11:50:35 +0200

    practicum 5 implementing array access operators

--- Dict/set.h
+++ Dict/set.h
@@ -1,24 +1,66 @@
 #ifndef set_h
 #define set_h
 
+#include "array"
+#include "iostream"
 
 class set {
 private:
     std::array<int, 10> entries;
+    std::array<bool, 10> occupied;
 
 public:
+    set() {
+        for (auto i: occupied) {
+            occupied[i] = false;
+        }
+    }
 
-    set();
+    std::array<int, 10> get_occupied_entries() {
+        unsigned int count = 0;
+        std::array<int, 10> res;
+        for (auto i: occupied) {
+            if (occupied[i]) {
+                res[count] = entries[i];
+                count++;
+            }
+        }
+        return res;
+    }
 
-    void add(int x);
+    void add(int x) {
+        if (contains( x )) return;
+        for (auto i: occupied) {
+            if (!occupied[i]) {
+                entries[i] = x;
+                occupied[i] = true;
+                return;
+            }
+        }
+    }
 
-    void remove(int x);
+    void remove(int x) {
+        for (auto i: entries) {
+            if (entries[i] == x) {
+                occupied[i] = false;
+                return;
+            }
+        }
+    }
 
-    bool contains(int);
-
-    std::ostream operator<<(std::ostream&, const set&);
-
+    bool contains(int x) {
+        for (auto i: entries) {
+            if (entries[i] == x) return true;
+        }
+        return false;
+    }
 };
+
+//std::ostream operator<<(std::ostream& lhs, set& rhs) {
+//    std::array<int, 10> x = rhs.get_occupied_entries();
+//
+//    return lhs << rhs.get_occupied_entries()[0];
+//}
 
 
 #endif //set_h
